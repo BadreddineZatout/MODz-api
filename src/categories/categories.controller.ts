01e96e4e42d612177f5f609d9d19f611eebd48e3
BadreddineZatout <hb_zatout@esi.dz>
2024-03-25 18:04:10 +0100

    refactor: remove category media

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,15 +1,5 @@
-import {
-  Controller,
-  Get,
-  Param,
-  ParseIntPipe,
-  Post,
-  UploadedFiles,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
-import { FilesInterceptor } from '@nestjs/platform-express';
-import { getStorageConfig } from 'src/utils/storage';
 
 @Controller('categories')
 export class CategoriesController {
@@ -18,18 +8,5 @@ export class CategoriesController {
   @Get('/')
   getCategories() {
     return this.categoriesService.getCategories();
-  }
-
-  @Post('/save-media/:id')
-  @UseInterceptors(
-    FilesInterceptor('files', 10, {
-      storage: getStorageConfig('public/categories'),
-    }),
-  )
-  saveMedia(
-    @UploadedFiles() files: Array<Express.Multer.File>,
-    @Param('id', ParseIntPipe) id: number,
-  ) {
-    return this.categoriesService.saveMedia(id, files);
   }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -8,27 +8,4 @@ export class CategoriesService {
   async getCategories() {
     return await this.prisma.category.findMany();
   }
-
-  async saveMedia(id: number, files: Array<Express.Multer.File>) {
-    const promises = files.map(async (file) => {
-      const media = await this.prisma.media.create({
-        data: {
-          name: file.filename,
-          path: file.destination + '/' + file.filename,
-          type: 'CATEGORY',
-        },
-      });
-      await this.prisma.category.update({
-        where: { id: id },
-        data: {
-          media: {
-            connect: { id: media.id },
-          },
-        },
-        include: {
-          media: true,
-        },
-      });
-    });
-  }
 }
