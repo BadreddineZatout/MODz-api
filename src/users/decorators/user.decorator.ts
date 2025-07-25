12d25d3dfcb4ad526dd771cf3c15d82e6558668f
BadreddineZatout <hb_zatout@esi.dz>
2025-07-25 21:26:09 +0100

    refactor: user decorator

--- src/users/decorators/user.decorator.ts
+++ src/users/decorators/user.decorator.ts
@@ -1,23 +1,15 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { PrismaService } from 'src/prisma.service';
 import { extractTokenFromHeader } from 'src/utils/token';
 
 export const User = createParamDecorator(
   async (data: unknown, ctx: ExecutionContext) => {
-    let prisma = new PrismaService();
     const jwtService = new JwtService();
     const request = ctx.switchToHttp().getRequest();
     const token = extractTokenFromHeader(request);
     const payload = await jwtService.verifyAsync(token, {
       secret: process.env.JWT_SECRET,
     });
-    const user = await prisma.user.findFirst({
-      where: {
-        id: payload.sub,
-      },
-    });
-    prisma = null;
-    return user.id;
+    return parseInt(payload.sub);
   },
 );
